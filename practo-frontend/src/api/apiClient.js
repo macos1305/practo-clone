57e7cb97c8b017143ab672c903c996f34b0dfdfb
macos1305 <manasa18106@gmail.com>
2026-06-09 22:23:31 +0530

    Add environment configuration

--- practo-frontend/src/api/apiClient.js
+++ practo-frontend/src/api/apiClient.js
@@ -2,10 +2,11 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
+
 
 // REQUEST INTERCEPTOR
 apiClient.interceptors.request.use(
